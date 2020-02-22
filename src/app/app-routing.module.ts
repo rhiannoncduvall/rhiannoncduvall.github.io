ed08ce62e0107d3ca0d7831994cd5a86671a8f32
rhiannoncduvall@gmail.com <rhiannoncduvall@gmail.com>
2020-02-21 21:41:36 -0800

    added content to home page and form to contact page (form and resume button not connected yet)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'projects', component: ProjectsComponent }
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent}
 ];
 
 @NgModule({
